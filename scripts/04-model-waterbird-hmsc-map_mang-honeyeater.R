7fbc1395c6ebf9524b5c045982f75fbefa93650d
cabuelow <christinabuelow@gmail.com>
2023-05-08 10:29:59 +1000

    update plotting of decreased prob. detection

--- scripts/04-model-waterbird-hmsc-map_mang-honeyeater.R
+++ scripts/04-model-waterbird-hmsc-map_mang-honeyeater.R
@@ -1,12 +1,13 @@
 # map threats and species' distributions
 
+library(Hmsc)
 library(sf)
 library(tmap)
 library(tidyverse)
 library(raster)
 library(abind)
 library(scales)
-library(Hmsc)
+library(caret)
 
 # load data
 
@@ -21,10 +22,13 @@ m
 # set predictor of interest to max of observed environmental gradient at our basins
 
 XDataNew <- data.frame(HYBAS_ID = dat$HYBAS_ID, m$XData) %>% 
-  mutate(Phosphorus_Loading = max(m$XData$Phosphorus_Loading))
+  mutate(Nitrogen_Loading = max(m$XData$Phosphorus_Loading))
 
 XDataNew2 <- data.frame(HYBAS_ID = dat$HYBAS_ID, m$XData) %>% 
   mutate(Consumptive_Water_Loss = max(m$XData$Consumptive_Water_Loss))
+
+XDataNewN <- data.frame(HYBAS_ID = dat$HYBAS_ID, m$XData) %>% 
+  mutate(Nitrogen_Loading = max(m$XData$Nitrogen_Loading))
 
 # make predictions
 
@@ -33,12 +37,12 @@ XDataNew2 <- data.frame(HYBAS_ID = dat$HYBAS_ID, m$XData) %>%
 predY <- predict(m, XData=m$XData, studyDesign=m$studyDesign,
                  ranLevels=m$ranLevels, expected=TRUE)
 
-# make predictions of spp. prob of occurrence where phosphorus is at max value, given known environmental gradient
-predY2 <- predict(m, XData=XDataNew, studyDesign=m$studyDesign,
-                 ranLevels=m$ranLevels, expected=TRUE)
+# make predictions of spp. prob of occurrence where CWL? is at max value, given known environmental gradient
+predY2 <- predict(m, XData=XDataNew2, studyDesign=m$studyDesign,
+                  ranLevels=m$ranLevels, expected=TRUE)
 
-# make predictions of spp. prob of occurrence where consumptive water loss is at max value, given known environmental gradient
-predY3 <- predict(m, XData=XDataNew2, studyDesign=m$studyDesign,
+# make predictions of spp. prob of occurrence where N? is at max value, given known environmental gradient
+predY3 <- predict(m, XData=XDataNewN, studyDesign=m$studyDesign,
                   ranLevels=m$ranLevels, expected=TRUE)
 
 # extract the mean prob. of occurrence from our posterior density distribution of predictions 
@@ -51,8 +55,9 @@ EpredY$HYBAS_ID <- XDataNew[,1]
 
 # calculating the predicted change in prob. of occurrence in a species given an increase threat
 # subtract the baseline probability of occurrence from our scenario-based prob. occurrence where we increase threat levels
-EpredY$Probability <- rescale(EpredY2$Black.Swan - EpredY$Black.Swan, to = c(0,1)) # rescale from 0-1 for easier visualisation
-EpredY$Probability2 <- rescale(EpredY3$Black.Swan - EpredY$Black.Swan, to = c(0,1))
+# except for nitrogen we expect a decrease in probability of occurrence, so subtract from baseline instead for nitrogen - so we get decreased prob. of detection from baseline
+EpredY$Probability <- rescale(EpredY2$Mangrove.Honeyeater - EpredY$Mangrove.Honeyeater, to = c(0,1)) # rescale from 0-1 for easier visualisation
+EpredY$Probability2 <- rescale(EpredY$Mangrove.Honeyeater - EpredY3$Mangrove.Honeyeater, to = c(0,1))
 
 # group the changes in prob. of occurrence into categories based on whether increased occurrence
 # is the same or different for different threats
@@ -70,7 +75,7 @@ ggplot(plotdf) +
   geom_point(aes(x = Probability, 
                  y = Probability2,
                  colour = group)) +
-  xlab('Probability of detection with increased Phosphorus') +
+  xlab('Probability of detection with increased Nitrogen') +
   ylab('Probability of detection with increased Consumptive Water Loss') +
   geom_vline(xintercept = 0.5) +
   geom_hline(yintercept = 0.5) +
@@ -79,7 +84,7 @@ ggplot(plotdf) +
   theme_classic() +
   theme(legend.position = 'none')
 
-ggsave('outputs/black-swan-scenario-correlation.png', width = 5.5, height = 5)
+ggsave('outputs/mangrove-honeyeater-scenario-correlation.png', width = 5.5, height = 5)
 
 # map predictions
 
@@ -88,25 +93,25 @@ predY.sf <- basins.qld %>%
 
 m1 <- tm_shape(qld) +
   tm_fill() + # can change this to tm_polygons if you want borders
-tm_shape(predY.sf) +
+  tm_shape(predY.sf) +
   tm_polygons('Probability')
 
 m2 <- tm_shape(qld) +
   tm_fill() +
-tm_shape(predY.sf) +
+  tm_shape(predY.sf) +
   tm_polygons('Probability2')
 
 mm <- tmap_arrange(m1, m2)
 mm
 
-tmap_save(mm, 'outputs/map-scenario_black-swan.png', width = 6.666, height = 4)
+tmap_save(mm, 'outputs/map-scenario_mangrove-honeyeater.png', width = 6.666, height = 4)
 
 m3 <- tm_shape(qld) +
   tm_fill() +
   tm_shape(predY.sf) +
   tm_polygons('group', palette = 'Set2', legend.show = F) # turn on legend by saying T
 m3
-tmap_save(m3, 'outputs/map-scenario_black-swan-categories.png', width = 6, height = 6)
+tmap_save(m3, 'outputs/map-scenario_mangrove-honeyeater-categories.png', width = 6, height = 6)
 
 ### End here
 
